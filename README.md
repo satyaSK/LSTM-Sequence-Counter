b5756e7c614b06c65658e0b3083f70253176e16f
satyaSK <satyajit.k@somaiya.edu>
2018-01-01 22:29:35 +0530

    Counting sequences of 1's using lstm

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# LSTM-Sequence-Counter
-Using LSTM's in tensorflow to calculate number of 1's in a generated binary element dataset
+# Counting sequences using LSTM's
+This is a short problem statement I found [monik's blog](http://monik.in/), which I wanted to try out. The aim is to create an RNN model which would simply output the number of 1's it had seen in a binary sequence containing ```input_sample_size``` number of elements. The main purpose of this exercise was to observe the effectiveness of a RNN at retaining information which it had encountered earlier in the sequence.
+
+## Dependencies
+* Tensorflow
+* Numpy
+* tqdm
+
+## Easy to visualize now?
+![rnn](https://user-images.githubusercontent.com/34591573/34469281-7c7ccb58-ef41-11e7-945e-7fc2e7e56675.png)
+This is an image I got from [Christopher Olah's blog](http://colah.github.io/).Notice how the information is being accumulated at each proceeding time step. Let's say that the A's are our hidden layer at each time step. So now, we are interested in collecting the hidden layer output produced at the last time step.
+
+## Simplified Approach
+* Firstly, data having a sequence length of ```input_sample_size``` is generated using the ```generate_data()``` function. Note that this data is evenly distributed over the whole sequence length. Therefore the probablity of encountering a "1" varies from 0 to 1(it is not centralized).
+* For example in a case where ```input_sample_size = 10``` the probablity of encountering a particular number of 1's is evenly distributed distributed over the whole dataset. Also note that the number of output will be 11 i.e ```n_classes = 11``` as the possible outputs are 0, 1, 2, 3 ...10.
+* The data is split into train-test set. I'm using 25% of the total data as my test set.
+* As there is a need for retaining long term sequences, I've used the LSTM cell to capture the sequence information.
+* LSTMs are well suited for time series data or sequential data, where the temporal dynamics of the system matters.
+* The input sequence of length ```time_steps``` is fed into the network. 
+* Here's where the real magic happens. When a sequence is fed into the hidden layer at every time step, this hidden layer passes the information into the next time step. This way temporal information accumulates in the hidden layer at every time step. The same hidden layer, at the last time step accumulates enough information inside what is called the "thought vector", to make a prediction.(make sure you understood every bit of this sentence)
+* As we only care about the final output only, we discard all the outputs except the one which we get after the whole sequence has been seen.
+* This final output is passed through the softmax function to give a probablity distribution over the number of 1's the network "thinks" it has seen in the sequence.
+* The prediction is made, the loss is calculated(cross-entropy) with respect to the weights, the loss is backpropagated over the ```truncated_backprop_length```(I haven't included this in the code), and finally the weights are updated.
+ 
+
+## Basic Usage
+For Running, type in terminal
+```
+python lstmCounter.py
+```
+For the beautiful visualization, type in terminal
+```
+tensorboard --logdir="visualize"
+```
+
+
+
---
+++ lstmCounter.py
@@ -0,0 +1,110 @@
+#problem statement on monik's page: http://monik.in/a-noobs-guide-to-implementing-rnn-lstm-using-tensorflow/
+import tensorflow as tf
+from tensorflow.contrib import rnn
+import numpy as np 
+input_sample_size = 20 #Use LSTM to count the number of 1's in a list of input_sample_size binary elements
+# fn to generate 100000 random lists(input_sample_size elements each) of binary elements
+def generate_data(input_sample_size):
+	x=[]
+	size = 100000
+	for i in range(125000):
+		a = np.random.randint(input_sample_size)/input_sample_size
+		b = 1 - a
+		sample = list(np.random.choice(2, input_sample_size, p=[a,b]))
+		x.append(sample)
+	
+	y=[]
+	for i in x:
+		count = int(np.sum(i))
+		y.append(count)
+	
+	one_hot = np.zeros((len(x),input_sample_size + 1), dtype=np.int8)
+	j = 0 #input_sample_size + 1 because there could be 21 outputs- from 0,1,2...20
+	
+	for i in one_hot:
+		i[y[j]] = 1
+		j += 1 
+
+	ti = np.array(x[:size])
+	to = np.array(one_hot[:size])
+	xi = np.array(x[size:len(x)])
+	xo = np.array(one_hot[size:len(x)])
+
+	return ti, to, xi, xo
+
+#get the generated data
+ti, to, xi, xo = generate_data(input_sample_size)
+print("Shape of training input :",ti.shape)
+print("Shape of training labels :",to.shape)
+
+#define hyperparameters
+keep_prob = 0.7 
+batch_size = 100
+time_steps = input_sample_size# list of 20 binary elements
+input_size = 1 
+num_units = 128 
+n_classes = input_sample_size + 1#again +1 is because number possible outputs = 21
+learning_rate = 0.001
+epochs = 4
+
+
+X = tf.placeholder(tf.float32, [None, time_steps, input_size], name='X')
+Y = tf.placeholder(tf.float32, [None, n_classes], name='Y')
+
+W2 = tf.Variable(tf.random_normal([num_units, n_classes]))
+b2 = tf.Variable(tf.random_normal([n_classes]))
+
+shapedInputs = tf.unstack(X, time_steps,1)# convert it into a list of tensor of shape [batch_size, input_size] of length time_steps which is the input taken by static_rnn
+
+
+cell = rnn.BasicLSTMCell(num_units, forget_bias = 1)# set the forgrt gate bias of the LSTM to 1(It is a default value but Im explicitly mentioning it)
+cell = tf.nn.rnn_cell.DropoutWrapper(cell, output_keep_prob=keep_prob)#this is the LSTM layer wrapped with dropout
+all_outputs, all_hidden_states = rnn.static_rnn(cell, shapedInputs, dtype='float32')
+
+prediction = tf.matmul(all_outputs[-1],W2) + b2
+
+entropy = tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels = Y)
+loss = tf.reduce_mean(entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+
+on_point=tf.equal(tf.argmax(prediction,1),tf.argmax(Y,1))
+training_accuracy=tf.reduce_mean(tf.cast(on_point,tf.float32))
+
+with tf.Session() as sess:
+    # Train the Model
+	sess.run(tf.global_variables_initializer())
+	writer = tf.summary.FileWriter("./visualize", sess.graph) 
+	num_batches = int(len(ti)/batch_size)
+	print("\nGood To Go - Training Starts\n")
+	for i in range(epochs):
+	 	epoch_loss = 0
+	 	s = 0
+	 	for j in range(num_batches):
+	 		X_batch, Y_batch = ti[s : s+batch_size], to[s : s+batch_size]
+	 		X_batch = X_batch.reshape((batch_size,time_steps, input_size))
+	 		e, _ = sess.run([loss, optimizer], feed_dict={X:X_batch,Y:Y_batch})
+	 		epoch_loss += e
+	 		s += batch_size
+	 		if j%100==0:
+	 			a = sess.run(training_accuracy, feed_dict={X:X_batch,Y:Y_batch})
+	 			print("Training accuracy percent now: ",a*100)
+	 	print("The total loss for EPOCH {0} is {1}".format(i+1,epoch_loss))
+	
+	#Testing the model
+
+	num_batches = int(len(xi)/batch_size)
+	overall_correct_preds = 0
+	s = 0
+	for k in range(num_batches):# you gotta create your own batches
+		X_batch, Y_batch = xi[s : s + batch_size], xo[s : s + batch_size]
+		X_batch = X_batch.reshape((batch_size,time_steps, input_size))
+		a = sess.run(training_accuracy, feed_dict={X:X_batch, Y:Y_batch}) 		
+		overall_correct_preds += a
+		s += batch_size
+		print("Test {0} Batch Accuracy:{1}".format(k,a*100))
+
+	print("Accuracy of Model = {0}".format((overall_correct_preds/num_batches)*100))
+	writer.close()
+	testIt = [[[1],[1],[1],[1],[1],[1],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0]]] #shape(?,20,1) aka (?,time_steps,input_size)
+	print(sess.run(prediction,feed_dict={X: testIt}))
+	print("Sum of 1's in sequence is: ",sess.run(tf.argmax(prediction,1),feed_dict={X: testIt}))	
